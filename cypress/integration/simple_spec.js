3379900d88c8fd18c617264792740ae034b4cf9c
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2019-09-29 22:52:58 -0500

    VRT con cypress, modificación de script para tomar screenshots

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -4,13 +4,15 @@ describe('Los estudiantes login', function() {
 		cy.contains('Cerrar').click()
 		//Lineas nuevas  
 		cy.contains('Ingresar').click()
+		cy.screenshot('IngresoFallido1')
 		cy.get('.cajaLogIn').find('input[name="correo"]').click().type("wrongemail@example.com")
 		cy.get('.cajaLogIn').find('input[name="password"]').click().type("1234")
 		cy.get('.cajaLogIn').contains('Ingresar').click()
 		cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
+		cy.screenshot('IngresoFallido2')
 		//Cerrar ventana de ingreso y de registro
 		cy.get('.cajaLogIn').find('input[name="password"]').click().type('{esc}')
-
+		
 
 		// Cree una cuenta  Registro exitoso!
 		/*cy.contains('Ingresar').click()
@@ -28,15 +30,20 @@ describe('Los estudiantes login', function() {
 
 		//pruebe el login correcto
 		cy.contains('Ingresar').click()
+		cy.screenshot('IngresoExitoso1')
 		cy.get('.cajaLogIn').find('input[name="correo"]').click().type("johannagut0@gmail.com")
 		cy.get('.cajaLogIn').find('input[name="password"]').click().type("Zaq12wsx*")
 		cy.get('.cajaLogIn').contains('Ingresar').click()
+		cy.screenshot('IngresoExitoso2')
 		//Cerrar sesión
 		cy.get('#cuenta').click()
 		cy.contains('Salir').click()
+		
 
 		// la creación de una cuenta con un login que ya existe
+		
 		cy.contains('Ingresar').click()
+		cy.screenshot('RegistroExistente1')
 		cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("Johanna")
 		cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("Gutierrez")
 		cy.get('.cajaSignUp').find('input[name="correo"]').click().type("johannagut0@gmail.com")
@@ -46,10 +53,10 @@ describe('Los estudiantes login', function() {
 		cy.get('.cajaSignUp').find('input[name="acepta"]').check()  
 		cy.get('.cajaSignUp').contains('Registrarse').click()
 		cy.contains('Ocurrió un error activando tu cuenta')
+		cy.screenshot('RegistroExistente2')
 		//Cerrar ventana de ingreso y de registro
 		cy.contains('Ok').click()
 		cy.get('.cajaLogIn').find('input[name="password"]').click().type('{esc}')
-
 
 		//Pruebe la funcionalidad de búsqueda de profesores y como dirigirse a la página de un profesor
 		cy.get('.Select-arrow-zone').click()
